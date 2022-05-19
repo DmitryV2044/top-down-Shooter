f511a16335f22563a4f1ccc61506047b84244b20
DmitryV2044 <dvovchinskiy@inbox.ru>
2022-05-19 15:00:54 +0500

    Создание базы для скриптов, создающих объекты в эдиторе

--- Assets/Editor/SpawnpointMenuItem.cs
+++ Assets/Editor/SpawnpointMenuItem.cs
@@ -1,17 +1,11 @@
 using UnityEditor;
 using UnityEngine;
 
-public class SpawnpointMenuItem : MonoBehaviour
+public class SpawnpointMenuItem : EditorObjectCreator
 {
-    [MenuItem("GameObject/Spawnpoints/Ninja Spawnpoint", priority = 2)]
+    private const string InitalPath = "GameObject/Spawnpoints/";
+
+    [MenuItem(InitalPath + "Ninja Spawnpoint", priority = 2)]
     private static void CreateNinjaSpawnPoint(MenuCommand menuCommand) => CreateCustomGameObject<NinjaSpawnpoint>("Ninja Spawnpoint", menuCommand);
 
-    private static void CreateCustomGameObject<T>(string name, MenuCommand menuCommand) where T : MonoBehaviour
-    {
-        GameObject go = new GameObject(name);
-        go.AddComponent<T>();
-        GameObjectUtility.SetParentAndAlign(go, menuCommand.context as GameObject);
-        Undo.RegisterCreatedObjectUndo(go, "Create " + go.name);
-        Selection.activeObject = go;
-    }
 }
